f108cf78636255d0cefdfa826ecc4a9e2add1831
Hristijan Petrovski <kiko-bt@hotmail.com>
2023-05-24 16:43:13 +0200

    Init

---
+++ src/HomePage.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React, { useState, useEffect } from "react";
+import { QuestionData, getUnansweredQuestions } from "./QuestionsData";
+import { QuestionList } from "./QuestionList";
+import Page from "./Page";
+import { PrimaryButton } from "./Styles";
+
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+
+  const handleAskQuestionClick = () => {
+    console.log("TODO - move to the AskPage");
+  };
+
+  return (
+    <Page>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        <h2>Unanswered Questions</h2>
+        <PrimaryButton onClick={handleAskQuestionClick}>
+          Ask a question
+        </PrimaryButton>
+        {questionsLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <QuestionList data={questions}></QuestionList>
+        )}
+      </div>
+    </Page>
+  );
+};
---
+++ src/Page.tsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React from "react";
+import { PageTitle } from "./PageTitle";
+
+interface Props {
+  title?: string;
+  children: React.ReactNode;
+}
+
+const Page = ({ title, children }: Props) => (
+  <div
+    css={css`
+      margin: 50px auto 20px auto;
+      padding: 30px 20px;
+      max-width: 600px;
+    `}
+  >
+    {title && <PageTitle>{title}</PageTitle>}
+    {children}
+  </div>
+);
+
+export default Page;
