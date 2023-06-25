d55329f53f259e07a1832b4572153effc445d8a3
Hristijan Petrovski <kiko-bt@hotmail.com>
2023-06-26 00:08:32 +0200

    Adding lazy and suspense for performance, example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AskPage } from "./AskPage";
 import { SearchPage } from "./SearchPage";
 import { SignInPage } from "./SignInPage";
 import { Header } from "./Header";
@@ -9,6 +9,8 @@ import { HomePage } from "./HomePage";
 import { fontFamily, fontSize, gray2 } from "./Styles";
 import { NotFoundPage } from "./NotFoundPage";
 import { QuestionPage } from "./QuestionPage";
+
+const AskPage = lazy(() => import("./AskPage"));
 
 function App() {
   return (
@@ -24,7 +26,25 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
